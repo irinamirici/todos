15ff7467a912a6d082fcffa35e901274b3d335bc
Irina <irinamirici@gmail.com>
2022-09-18 13:50:54 +0200

    Cleanup data seed

--- SeedData/Program.cs
+++ SeedData/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
-using Todos.SeedData;
 using Todos.Persistence.Domain;
+using Todos.Persistence.Configuration;
 using Azure.Search.Documents;
 using Azure.Search.Documents.Models;
 using Azure.Search.Documents.Indexes;
@@ -21,30 +21,23 @@ var configurationRoot = builder.Build();
 
 var config = configurationRoot.GetSection(nameof(SearchConfiguration)).Get<SearchConfiguration>();
 
-Console.WriteLine(config.Endpoint);
-const string SEARCH_INDEX_NAME = "todos1";
 Uri searchEndpointUri = new Uri(config.Endpoint);
-
-SearchClient client = new SearchClient(
-    searchEndpointUri,
-    SEARCH_INDEX_NAME,
-    new AzureKeyCredential(config.ApiKey));
 
 SearchIndexClient clientIndex = new SearchIndexClient(
     searchEndpointUri,
     new AzureKeyCredential(config.ApiKey));
 
-CreateIndexAsync(clientIndex).Wait();
-BulkInsertAsync(client).Wait();
+CreateIndexAsync(clientIndex, config.IndexName).Wait();
+BulkInsertAsync(clientIndex.GetSearchClient("todos1")).Wait();
 
-static async Task CreateIndexAsync(SearchIndexClient clientIndex)
+static async Task CreateIndexAsync(SearchIndexClient clientIndex, string indexName)
 {
     Console.WriteLine("Creating (or updating) search index");
     // TODO const string CORS = "https://portal.azure.com";
     var cors = new CorsOptions(new List<string> { "*" });
     cors.MaxAgeInSeconds = 300;
     FieldBuilder builder = new FieldBuilder();
-    var index = new SearchIndex(SEARCH_INDEX_NAME)
+    var index = new SearchIndex(indexName)
     {
         Fields = builder.Build(typeof(Todo)),
         CorsOptions = cors
@@ -57,18 +50,18 @@ static async Task CreateIndexAsync(SearchIndexClient clientIndex)
 static async Task BulkInsertAsync(SearchClient client)
 {
     var todos = new List<Todo>{
-        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Feed the Cat", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
-        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Buy cat food", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
-        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Laundry", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
-        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Feed the Cat", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
-        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Buy cat food", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
-        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Laundry", IsDone = false, CreatedAt = DateTimeOffset.UtcNow}
+        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Test 1", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
+        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Test 2", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
+        new Todo{ Id = Guid.NewGuid().ToString(), Description = "Dishes", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
+        new Todo{ Id = Guid.NewGuid().ToString(), Description = "test 3", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
+        new Todo{ Id = Guid.NewGuid().ToString(), Description = "4 test", IsDone = false, CreatedAt = DateTimeOffset.UtcNow},
+        new Todo{ Id = Guid.NewGuid().ToString(), Description = "5 test", IsDone = false, CreatedAt = DateTimeOffset.UtcNow}
     };
 
-    Console.WriteLine("Uploading bulk data");
+    Console.WriteLine("Uploading data");
     _ = await client.UploadDocumentsAsync(todos);
 
     var docCount = (int)await client.GetDocumentCountAsync();
 
-    Console.WriteLine($"Finished bulk inserting data. Count: {docCount}");
+    Console.WriteLine($"Finished inserting data. Count: {docCount}");
 }
--- SeedData/SearchConfiguration.cs
+++
@@ -1,7 +0,0 @@
-namespace Todos.SeedData;
-
-public class SearchConfiguration
-{
-    public string Endpoint { get; set; } = null!;
-    public string ApiKey { get; set; } = null!;
-}
