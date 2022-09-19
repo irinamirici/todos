95505f1c5a74e4e15d329d67e462dc8743ae087e
Irina <irinamirici@gmail.com>
2022-09-19 14:24:57 +0200

    Add fuzzy search for multiple terms

--- Persistence/Domain/SearchCriteria.cs
+++ Persistence/Domain/SearchCriteria.cs
@@ -2,6 +2,18 @@ namespace Todos.Persistence.Domain;
 
 public class SearchCriteria
 {
+    /// <summary>
+    /// Separators to split words in the searched phrase by
+    /// </summary>
+    private readonly char[] separators = new[] { ' ', ',', ';', '.' };
+    private const string FUZZY_WORD_JOIN = " AND ";
+    private const string FUZZY_SEARCH_INDICATOR = "~";
+
+    /// <summary>
+    /// Performs an empty fuzzy search, 
+    /// i.e. it retrieves all results, as if no search term was provided
+    /// </summary>
+    private const string EMPTY_SEARCH = "*";
     public SearchCriteria(string? search, int page, int itemsPerPage)
     {
         this.SearchText = search;
@@ -13,17 +25,24 @@ public class SearchCriteria
     public int Page { get; init; }
     public int ItemsPerPage { get; init; }
 
+    /// <summary>
+    /// Builds a full Lucene syntax search expression, performing Fuzzy search for each word
+    /// Search distance is 2 (default)
+    /// </summary>
     public string FuzzySearchTerm
     {
         get
         {
             if (string.IsNullOrWhiteSpace(this.SearchText))
             {
-                return "*"; // get everything
+                return EMPTY_SEARCH; // get everything
             }
 
-            // TODO - split words and add fuzzy for each
-            return this.SearchText + "~"; // fuzzy
+            // split words and add fuzzy search indicator (~) for each
+            var words = this.SearchText.Split(this.separators,
+                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            return string.Join(FUZZY_WORD_JOIN, words.Select(x => x + FUZZY_SEARCH_INDICATOR));
         }
     }
 
